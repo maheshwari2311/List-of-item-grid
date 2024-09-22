658cfafaa8f75b8ff0f02307885d7d65bccb4d87
Kunal Maheshwari <108612650+maheshwari2311@users.noreply.github.com>
2024-09-22 09:52:59 +0530

    Add files via upload

---
+++ lib/list.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class MyHome extends StatefulWidget {
+  const MyHome({super.key});
+
+  @override
+  State<MyHome> createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "List grid",
+        ),
+      ),
+      body: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+          itemCount: 20,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+                child: Center(
+              child: Text(
+                'item $index',
+              ),
+            ));
+          }),
+    );
+  }
+}
